d80f34301a92c8bb850ac6fac3c91dbf1df41426
gradispo <gradispo@gmail.com>
2022-09-26 08:52:33 +0900

    no message

--- src/PhoneManager.java
+++ src/PhoneManager.java
@@ -6,9 +6,9 @@ public class PhoneManager {
 
         Scanner scanner = new Scanner(System.in);
         System.out.print("인원수>>");
-        int Person = scanner.nextInt();
+        int person = scanner.nextInt();
 
-        Phone2[] Array = new Phone2[Person];
+        Phone2[] Array = new Phone2[person];
         for (int i=0; i<Array.length; i++) {
             System.out.print("이름과 전화번호(번호는 연속적으로 입력>>");
             Array[i] = new Phone2(scanner.next(), scanner.next());
@@ -17,17 +17,17 @@ public class PhoneManager {
 
         label : while(true) {
             System.out.print("검색할 이름>>");
-            String Name = scanner.next();
+            String name = scanner.next();
             
-            for (int i=0; i< Array.length; i++) {
-                if(Name.equals("exit")) {
+            for (int i=0; i< person; i++) {
+                if(name.equals("exit")) {
                     break label;
                 }
-                else if (Name.equals(Array[i].getName())) {
+                else if (name.equals(Array[i].getName())) {
                     System.out.println(Array[i].getName() + "의 번호는 " + Array[i].getTel() + "입니다.");
                 }
-                else if (!Name.equals(Array[i].getName()) && i == (Person-1)) {
-                    System.out.println(Name + "이(가) 없습니다.");
+                else if (!name.equals(Array[i].getName()) && i == (person-1)) {
+                    System.out.println(name + "이(가) 없습니다.");
                 }
 
 //                !word.equals(eng[i]) && i == 5
